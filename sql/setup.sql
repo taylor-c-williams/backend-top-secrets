e47f7ef951b3cf1c9bf443b9b6994a5c14e89f64
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-02-07 16:48:42 -0800

    first draft users & secrets tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+-- Two Tables: Users and Secrets
+
+-- Users:
+-- Email
+-- Hashed Password
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE secrets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );
