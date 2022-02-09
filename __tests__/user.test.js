fc032d68d05523762b52a26ccc0923d1df5086b8
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-02-08 16:01:00 -0800

    log in functionality implemented, passing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -37,24 +37,32 @@ describe('User route tests', () => {
   });
 
   it('creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(mockUser);
-
+    const res = await request(app).post('/api/v1/users/register').send(mockUser);
     const { email } = mockUser;
-
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
     });
   });
 
-  // it('logs in a mock user', async () => {
+  it('logs in a mock user', async () => {
+    const [agent] = await registerAndLogin();
+    const sessions = await agent.post('/api/v1/users/login').send(mockUser);
+    expect(sessions.body).toEqual({
+      message: 'Log-in Successful'
+    });
+  });
+
+  // it('logs out a mock user by deleting a session', async () => {
   //   const [agent, user] = await registerAndLogin();
   //   const sessions = await agent.get('/api/v1/sessions');
+  //   console.log(sessions.body);
   //   expect(sessions.body).toEqual({
   //     ...user,
   //     exp: expect.any(Number),
   //     iat: expect.any(Number),
   //   });
+
   // });
 
 });
